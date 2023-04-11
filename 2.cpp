1e7e46e8ba174fcc4347a3d5942b133b48f5eff2
shengzikun <shengzikun@126.com>
2023-04-11 16:26:06 +0800

    1

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    for(a=1;a<10;a++)
+    {
+        for(b=1;b<=a;b++)
+        {
+            printf("%d*%d=%-3d",a,b,a*b);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
