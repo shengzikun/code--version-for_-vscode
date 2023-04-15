28d747297dc319bb30c1b35a8b8d14074086d5d9
shengzikun <shengzikun@126.com>
2023-04-15 23:43:44 +0800

    1

--- 2.cpp
+++ 2.cpp
@@ -4,22 +4,21 @@ int main()
 int password=0;
 while((password=getchar())!=EOF)
 {
-    if(password!='\n')
+    if(password='\n')
     {
      printf("成功\n");
      printf("退出");
      break;
-    }
-  
+    } 
     else
     {
      printf("失败\n");
-     continue;
+    
     }
-while((password=getchar())!='\n')
-        {
-            ;
-        }  
+// while((password=getchar())!='\n')
+//         {
+//             ;
+//         }  
       
 }
 
