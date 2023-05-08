8bc21258d4ea3e5b91b44bf26c1acdfbe2a9101a
shengzikun <106431581+shengzikun@users.noreply.github.com>
2023-05-08 15:07:29 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,12 @@
-for i in range(1,10):
-    for j in range(1,i+1):
-        result= j*i
-        print("{}×{}={}".format(j,i,result),end=' ')
-    print()
+#for i in range(1,10):
+ #   for j in range(1,i+1):
+#        result= j*i
+#        print("{}×{}={}".format(j,i,result),end=' ')
+#    print()
 
+
+from pyecharts.charts import Bar
+bar=Bar()
+bar.add_xaxis([1,2,3,4,5,6,])
+bar.add_yaxis("商家a",[5,20,36,10,75,90])
+bar.render()
