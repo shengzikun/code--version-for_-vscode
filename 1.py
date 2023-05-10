3a2d67041c3d0bd7402500e848c39883fe107976
shengzikun <106431581+shengzikun@users.noreply.github.com>
2023-05-10 10:57:33 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,10 +3,21 @@
 #        result= j*i
 #        print("{}×{}={}".format(j,i,result),end=' ')
 #    print()
+#============
 
+# from pyecharts.charts import Bar
+# bar=Bar()
+# bar.add_xaxis([1,2,3,4,5,6,])
+# bar.add_yaxis("商家a",[5,20,36,10,75,90])
+# bar.render()
 
-from pyecharts.charts import Bar
-bar=Bar()
-bar.add_xaxis([1,2,3,4,5,6,])
-bar.add_yaxis("商家a",[5,20,36,10,75,90])
-bar.render()
+# import random
+from random import randint
+
+yz=''
+for i in range(6):
+    # a=random.randint(0,9)
+    a=randint(0,9)
+    yz=yz+str(a)
+
+print(yz)    
