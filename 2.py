749388a4427caa63479952c2f2cd7ebe6b76628b
shengzikun <shengzikun@126.com>
2023-06-06 13:14:48 +0800

    342

--- 2.py
+++ 2.py
@@ -1,11 +1,12 @@
-so=open("2.txt","rt","en", coding='UTF-8')
-txt=so.read()
-txt=txt.lower()
+# so=open("2.txt","rt","en", coding='UTF-8')
+# txt=so.read()
+# txt=txt.lower()
 
-str1=".','--=()"
-for ch in str1:
-    txt=txt.replace(ch,"")
+# str1=".','--=()"
+# for ch in str1:
+#     txt=txt.replace(ch,"")
 
-word=txt.split()
-counts={}
+# word=txt.split()
+# counts={}
+
 
