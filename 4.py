6da0b1b04c3397c4d3ab756f5f2602be82cf8548
shengzikun <shengzikun@126.com>
2023-08-06 15:33:57 +0800

    33

--- 4.py
+++ 4.py
@@ -1,6 +1,6 @@
 
 import urllib.request
-url="https://weibo.com/hot"
+url="https://jwpd.webvpn.jsei.edu.cn/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su=2022220961t&ps=2jsei0961"
 
 html=urllib.request.urlopen(url)
 
