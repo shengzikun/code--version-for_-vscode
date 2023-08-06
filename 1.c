e910c607a08ca2fe6d042bd573452c663825f504
shengzikun <shengzikun@126.com>
2023-08-06 16:37:22 +0800

    2

--- 1.c
+++ 1.c
@@ -583,9 +583,34 @@ if(a=2)
 	
 // 	return 0;
 // } 
-//========================
+//冒泡排序===============================
+void bubble_sort(int arr[])
+{
+	int i=0;
+	int sz = sizeof (arr) / sizeof(arr[0]);
+	for(i=0;i=sz-1;i++)
+      {
+		int j=0;
+		for(j=0;j<sz-1-i;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int tmp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=tmp;
+			}
+		}
+	  }
+}
 int main()
 {
-	
+	int arr[]={9,8,7,6,5,4,3,2,1,0};
+	int i=0;
+	int sz=sizeof (arr) / sizeof(arr[0]);
+	bubble_sort(arr);
+	for(i=0;i<sz;i++)
+	{
+		printf("%d",arr[i]);
+	}
 	return 0;
 }
